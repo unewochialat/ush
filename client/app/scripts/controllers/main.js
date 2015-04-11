fcdc8342df1dcc32927696a7e666c6b110c2d565
Yves Schleich <yves@Yvess-MacBook-Air.local>
2015-04-11 22:55:28 +0200

    Deleted redundant rows

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -7,13 +7,10 @@
  * # MainCtrl
  * Controller of the spaceappsApp
  */
+
+
 angular.module('spaceappsApp')
         .controller('MainCtrl', function ($scope, $http, $log, uiGmapGoogleMapApi) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
 
         $scope.method = 'GET';
         $scope.url = 'http://space.lasse-nielsen.de/api?resp=json&task=incidents';
@@ -64,69 +61,6 @@ angular.module('spaceappsApp')
                 });
 
                 $log.log($scope.markers);
-            });
-        };
-    });
-
-//'use strict';
-
-/**
- * @ngdoc function
- * @name spaceappsApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the spaceappsApp
- */
-angular.module('spaceappsApp')
-    .controller('MainCtrl', function ($scope, $http, $log, uiGmapGoogleMapApi) {
-        $scope.method = 'GET';
-        $scope.url = 'http://space.lasse-nielsen.de/api?resp=json&task=incidents';
-
-        $http.get($scope.url).
-            then(function(result) {
-                $scope.initMap(result.data);
-            }).catch(function() {
-                console.log('Error');
-            })
-            .finally(function() {
-                console.log('Finished');
-            });
-
-
-        $scope.map = {
-            doCluster: true,
-            center: {
-                latitude: 50.124694,
-                longitude: 8.706254
-            },
-            zoom: 8,
-            bounds: {}
-        };
-
-        $scope.initMap = function(data) {
-            var locations = data.payload.incidents;
-            $scope.markers = [];
-
-            uiGmapGoogleMapApi.then(function() {
-
-                var lat, lon;
-                var title;
-
-                angular.forEach(locations, function(location, index){
-
-                    lat = location.incident.locationlatitude;
-                    lon = location.incident.locationlongitude;
-                    title = location.incident.locationname;
-
-                    if(!!lat) {
-                        $scope.markers.push({
-                            id: index,
-                            latitude: parseFloat(lat),
-                            longitude: parseFloat(lon),
-                            title: 'm' + index
-                        }) ;
-                    }
-                });
             });
         };
     });
