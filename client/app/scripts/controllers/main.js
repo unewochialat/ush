7d5649b15179ce2cdb8f8ce607ddadc1672b3a85
Yves Schleich <yves@Yvess-MacBook-Air.local>
2015-04-12 09:39:10 +0200

    Added Header and Parallax Style

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,23 +8,235 @@
  * Controller of the spaceappsApp
  */
 
+var mapStyle = [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#f00b0b"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#787878"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#2e2e2e"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#c91515"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#da2020"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#611c1c"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#973a3a"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#0f252e"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    }
+];
+
 
 angular.module('spaceappsApp')
-        .controller('MainCtrl', function ($scope, $http, $log, uiGmapGoogleMapApi) {
+    .controller('MainCtrl', function ($scope, $http, $log, uiGmapGoogleMapApi) {
 
         $scope.method = 'GET';
         $scope.url = 'http://space.lasse-nielsen.de/api?resp=json&task=incidents';
 
         $http.get($scope.url).
-            then(function(result) {
+            then(function (result) {
                 $scope.initMap(result.data);
-            }).catch(function() {
+            }).catch(function () {
                 console.log('Error');
             })
-            .finally(function() {
+            .finally(function () {
                 console.log('Finished');
             });
-
 
         $scope.map = {
             doCluster: true,
@@ -33,34 +245,39 @@ angular.module('spaceappsApp')
                 longitude: 8.706254
             },
             zoom: 8,
-            bounds: {}
+            bounds: {},
+            options: {
+                scrollwheel: false,
+                styles: mapStyle
+            }
         };
 
-        $scope.initMap = function(data) {
+        $scope.initMap = function (data) {
             var locations = data.payload.incidents;
             $scope.markers = [];
 
-            uiGmapGoogleMapApi.then(function() {
+            uiGmapGoogleMapApi.then(function () {
 
                 var lat, lon;
                 var title;
 
-                angular.forEach(locations, function(location, index){
+                angular.forEach(locations, function (location, index) {
 
                     lat = location.incident.locationlatitude;
                     lon = location.incident.locationlongitude;
                     title = location.incident.locationname;
 
-                    if(!!lat) {
+                    if (!!lat) {
                         $scope.markers.push({
                             id: index,
                             latitude: lat,
                             longitude: lon
-                        }) ;
+                        });
                     }
                 });
-
-                $log.log($scope.markers);
             });
         };
+
+        $.stellar();
+
     });
